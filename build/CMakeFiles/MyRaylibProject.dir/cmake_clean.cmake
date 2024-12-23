7f27b668d743b229fd7ebee990f32bf9694c49c8
Maks <csgo20041@gmail.com>
2024-12-23 23:59:07 +0100

    batman <_>

---
+++ build/CMakeFiles/MyRaylibProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyRaylibProject.dir/link.d"
+  "CMakeFiles/MyRaylibProject.dir/src/main.cpp.o"
+  "CMakeFiles/MyRaylibProject.dir/src/main.cpp.o.d"
+  "MyRaylibProject"
+  "MyRaylibProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyRaylibProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
